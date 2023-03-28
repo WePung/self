b10c245bb7cd1fbe11941a15f3ff38e55d437c83
WePung <akgre5@naver.com>
2023-03-28 23:21:20 +0900

    React API호출

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
 import DiaryEditor from './DiaryEditor';
 import DiaryList from './DiaryList';
-import LifeCycle from './LifeCycle';
+
+// https://jsonplaceholder.typicode.com/comments
 
 
 function App() {
+
+  const getData = async() => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/comments"
+  ).then(
+    (res)=>res.json()
+    );
+    const initData = res.slice(0, 20).map((it)=>{
+      return{
+        author: it.email,
+        content: it.body,
+        emotion: Math.floor(Math.random() * 5)+1,
+        created_data : new Date().getTime,
+        id : dataId.current++
+      }
+    })
+
+    setData(initData);
+  };
+
+  useEffect(()=>{
+    getData();
+  },[])
 
   const [data, setData] = useState([]);
 
@@ -64,7 +87,6 @@ const onEdit = (targetId, newContent) =>{
 
   return (
     <div className="App">
-      <LifeCycle />
       <DiaryEditor
       onCreate = {onCreate}
       />
@@ -72,7 +94,6 @@ const onEdit = (targetId, newContent) =>{
       diaryList = {data}
       onEdit={onEdit}
       onRemove = {onRemove}
-
       />
     </div>
   );
