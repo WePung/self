dd4151e173e49331f1b69cb04dc6875cb0750d44
WePung <akgre5@naver.com>
2023-03-30 00:22:56 +0900

    OptimizeTest

--- src/OptimizeTest.js
+++ src/OptimizeTest.js
@@ -1,39 +1,61 @@
 import React, { useEffect, useState } from 'react';
 
+const CounterA = React.memo(
+    ({count}) =>{
+        useEffect(()=>{
+            console.log(`CounterA UpDate:${count}`)
+        })
+        return <div>{count}</div>
+    }
+);
+
+const CounterB = ({object}) =>{
+        useEffect(()=>{
+            console.log(`CounterB UpDate:${object.count}`)
+        });
+        return <div>{object.count}</div>
+};
+
+const areEqual = (prevProps, nextProps) => {
+    if(prevProps.object.count === nextProps.object.count){
+        return true;
+    }
+    return false;
+}
+
+const MemoizedCounterB = React.memo(CounterB, areEqual);
+
 const OptimizeTest = () => {
 
     const [count, setCount] = useState(1);
-    const [text, setText] = useState("");
+    const [object, setObject] = useState({
+        count:1
+    });
 
-    const onCount = () =>{
-        setCount(count+1);
-    }
-    const onText = (e) =>{
-        setText(e.target.value);
-    }
-    const CountView = ({count})=>{
-        useEffect(()=>{
-            console.log(count);
-        },[])
-        return <div>{count}</div>
-    }
-    const TextView = ({text})=>{
-        useEffect(()=>{
-            console.log(text);
-        },[])
-        return <div>{text}</div>
-    }
+   
 
-    
+    const onCountA = () =>{
+        setCount(count);
+    };
+
+    const onCountB = () =>{
+        setObject({
+            count: object.count
+        });
+    }
 
     return (
         <div>
-            <h2>Count</h2>
-            <CountView count={count}/>
-            <button onClick={onCount}>증가</button>
-            <h2>Text</h2>
-            <TextView text={text}/>
-            <input type={text} value={text} onChange={onText}></input>
+            <div>
+                <h2>CountA</h2>
+                <CounterA count={count}/>
+                <button onClick={onCountA}>A 버튼</button>
+            </div>
+            <div>
+                <h2>CountB</h2>
+                <MemoizedCounterB object={object}/>
+                <button onClick={onCountB}>B 버튼</button>
+            </div>
         </div>
     );
 };
