d304e019c8b47b46783f66c42bdb12e54ce70f19
WePung <akgre5@naver.com>
2023-04-02 02:06:15 +0900

    filter
    오래된 순, 최신순

--- src/componet/DiaryList.js
+++ src/componet/DiaryList.js
@@ -1,9 +1,60 @@
-import React from 'react';
+import React,{useState} from 'react';
+
+const sortOptionList = [
+    {
+        value:"lastest",
+        name : "최신순", 
+    },
+    {
+        value:"oldest",
+        name : "오래된 순", 
+    }
+]
+
+const ControlMenu = ({value, onChange, optionList}) =>{
+    return(
+        <select
+        value={value}
+        onChange={(e)=> onChange(e.target.value)}
+        >
+            {optionList.map((it, idx)=>
+            <option key={idx}>
+                {it.name}
+            </option>
+            )
+            }
+        </select>
+    )
+}
 
 const DiaryList = ({diaryList}) => {
+
+    const [sortType, setSortType] = useState("lastest");
+
+    const getProcessedDiaryList = () => {
+        const compare = (a, b)=>{
+            if(sortType === "lastest"){
+                console.log("h1")
+                return parseInt(b.date) - parseInt(a.date);
+            }else{
+                console.log("h2")
+                return parseInt(a.date) - parseInt(b.date);
+            }
+        };
+
+        const copyList = JSON.parse(JSON.stringify(diaryList)) // JSON.stringigy()안의 값을 JSON화 시켜 문자열로 반환 후 JSON.parse를 통해 복구를 함 즉 값만 저장됨
+        const sortedList = copyList.sort(compare); // sort()는 배열 정렬하는 함수
+        return sortedList;
+    };
+
     return (
         <div>
-            {diaryList.map((it)=>(
+            <ControlMenu
+            value={sortType}
+            onChange={setSortType}
+            optionList={sortOptionList}
+            />
+            {getProcessedDiaryList().map((it)=>(
             <div key = {it.id}>
                 {it.content}
             </div>
@@ -11,5 +62,9 @@ const DiaryList = ({diaryList}) => {
         </div>
     );
 };
+
+DiaryList.defaultPorps = {
+    diaryList: []
+}
 
 export default DiaryList;
--- src/diary/App.js
+++ src/diary/App.js
@@ -1,115 +1,115 @@
 
-// import React, { useCallback, useEffect, useMemo, useReducer, useRef, useState } from 'react';
-// import DiaryEditor from './DiaryEditor';
-// import DiaryList from './DiaryList';
-// import OptimizeTest from './OptimizeTest';
+import React, { useCallback, useEffect, useMemo, useReducer, useRef, useState } from 'react';
+import DiaryEditor from './DiaryEditor';
+import DiaryList from './DiaryList';
+import OptimizeTest from './OptimizeTest';
 
-// const reducer = (state, action) =>{
-//   switch(action.type){
-//     case 'INIT' :{
-//       return action.data
-//     }
-//     case 'CREATE' :{
-//       const created_data = new Date().getTime();
-//       const newItem = {
-//         ...action.data,
-//         created_data
-//       }
-//       return [newItem, ...state];
-//     }
-//     case 'REMOVE' :{
-//       return state.filter((it)=>it.id !== action.targetId);
-//     }
-//     case 'EDIT' :{
-//       return state.map((it)=> it.id === action.targetId ? {...it, content:action.newContent}: it)
-//     }
-//     default : 
-//     return state;
-//   }
+const reducer = (state, action) =>{
+  switch(action.type){
+    case 'INIT' :{
+      return action.data
+    }
+    case 'CREATE' :{
+      const created_data = new Date().getTime();
+      const newItem = {
+        ...action.data,
+        created_data
+      }
+      return [newItem, ...state];
+    }
+    case 'REMOVE' :{
+      return state.filter((it)=>it.id !== action.targetId);
+    }
+    case 'EDIT' :{
+      return state.map((it)=> it.id === action.targetId ? {...it, content:action.newContent}: it)
+    }
+    default : 
+    return state;
+  }
 
-// }
+}
 
-// export const DiaryStateContext = React.createContext();
+export const DiaryStateContext = React.createContext();
 
-// export const DiaryDispatchContext = React.createContext();
+export const DiaryDispatchContext = React.createContext();
 
-// function App() {
+function App() {
 
-//   const [data, dispatch] = useReducer(reducer, []);
+  const [data, dispatch] = useReducer(reducer, []);
 
-//   const getData = async() => {
-//   const res = await fetch("https://jsonplaceholder.typicode.com/comments"
-//   ).then(
-//     (res)=>res.json()
-//     );
-//     const initData = res.slice(0, 20).map((it)=>{
-//       return{
-//         author: it.email,
-//         content: it.body,
-//         emotion: Math.floor(Math.random() * 5)+1,
-//         created_data : new Date().getTime,
-//         id : dataId.current++
-//       }
-//     })
-//     dispatch({type:'INIT', data:initData});
-//   };
+  const getData = async() => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/comments"
+  ).then(
+    (res)=>res.json()
+    );
+    const initData = res.slice(0, 20).map((it)=>{
+      return{
+        author: it.email,
+        content: it.body,
+        emotion: Math.floor(Math.random() * 5)+1,
+        created_data : new Date().getTime,
+        id : dataId.current++
+      }
+    })
+    dispatch({type:'INIT', data:initData});
+  };
 
-//   useEffect(()=>{
-//     getData();
-//   },[])
+  useEffect(()=>{
+    getData();
+  },[])
 
-//   const dataId = useRef(0);
+  const dataId = useRef(0);
 
-//   const onCreate = (author, content, emotion) =>{
+  const onCreate = (author, content, emotion) =>{
 
-//     dispatch({type:"CREATE", data:{author, content, emotion, id: dataId.current}})
+    dispatch({type:"CREATE", data:{author, content, emotion, id: dataId.current}})
 
-//     dataId.current += 1;
-//   }
-//   const onRemove = useCallback((targetId) =>{
-//     dispatch({type:"REMOVE", targetId} )
-// },[]);
+    dataId.current += 1;
+  }
+  const onRemove = useCallback((targetId) =>{
+    dispatch({type:"REMOVE", targetId} )
+},[]);
 
-//   const onEdit = useCallback((targetId, newContent) =>{
-//     dispatch({type:"EDIT", targetId, newContent})
-//   },[]);
+  const onEdit = useCallback((targetId, newContent) =>{
+    dispatch({type:"EDIT", targetId, newContent})
+  },[]);
 
-//   const memoizedDispatch = useMemo(()=>{
-//     return {onCreate, onRemove, onEdit}
-//   }, []);
+  const memoizedDispatch = useMemo(()=>{
+    return {onCreate, onRemove, onEdit}
+  }, []);
 
-//   const  getDiaryAnalysis = useMemo(
-//     () => {
-//     console.log("일기 분석 시작");
+  const  getDiaryAnalysis = useMemo(
+    () => {
+    console.log("일기 분석 시작");
 
-//     const goodCount = data.filter((it)=>it.emotion >= 3).length;
-//     const badCount = data.length - goodCount;
-//     const goodRatio = (goodCount / data.length) * 100;
-//     return {goodCount, badCount, goodRatio};
-//   },[data.length]
-//   );
+    const goodCount = data.filter((it)=>it.emotion >= 3).length;
+    const badCount = data.length - goodCount;
+    const goodRatio = (goodCount / data.length) * 100;
+    return {goodCount, badCount, goodRatio};
+  },[data.length]
+  );
 
-//   const {goodCount, badCount, goodRatio} = getDiaryAnalysis;
+  const {goodCount, badCount, goodRatio} = getDiaryAnalysis;
 
-//   return (
-//     <DiaryStateContext.Provider value={data}>
-//     <DiaryDispatchContext.Provider value={memoizedDispatch}>
-//     <div className="App">
-//       <DiaryEditor
-//       onCreate = {onCreate}
-//       />
-//       <div>전체 일기:{data.length}</div>
-//       <div>기분 좋은 일기:{goodCount}</div>
-//       <div>기분 나쁜 일기:{badCount}</div>
-//       <div>기분 좋은 일기 비율:{goodRatio}</div>
-//       <DiaryList
-//       onEdit={onEdit}
-//       onRemove = {onRemove}
-//       />
-//     </div>
-//     </DiaryDispatchContext.Provider>
-//     </DiaryStateContext.Provider>
-//   );
-// }
+  return (
+    <DiaryStateContext.Provider value={data}>
+    <DiaryDispatchContext.Provider value={memoizedDispatch}>
+    <div className="App">
+      <DiaryEditor
+      onCreate = {onCreate}
+      />
+      <div>전체 일기:{data.length}</div>
+      <div>기분 좋은 일기:{goodCount}</div>
+      <div>기분 나쁜 일기:{badCount}</div>
+      <div>기분 좋은 일기 비율:{goodRatio}</div>
+      <DiaryList
+      onEdit={onEdit}
+      onRemove = {onRemove}
+      />
+    </div>
+    </DiaryDispatchContext.Provider>
+    </DiaryStateContext.Provider>
+  );
+}
 
-// export default App;
+export default App;
